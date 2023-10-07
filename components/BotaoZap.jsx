5a2b5e2da7b60f14475d48b90b557062eec51119
João Vitor de Aquino <jvaquinocontact@gmail.com>
2023-10-07 10:11:24 -0300

    ~ diminuir botaoZap

--- components/BotaoZap.jsx
+++ components/BotaoZap.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 function BotaoZap() {
   return ( 
-    <Link className="fixed bottom-[18px] right-[18px] z-20 rounded-full h-[67px] w-[67px] lg:h-[82px] lg:w-[82px] divImage full" style={{ backgroundImage: "url(/images/zapLogo.webp)" }} href="https://wa.me/12987024097?text=Olá!+Eu+gostaria+de+saber+mais+sobre+os+seus+produtos"></Link>
+    <Link className="fixed bottom-[18px] right-[19px] z-20 rounded-full h-[64px] w-[64px] lg:h-[75px] lg:w-[75px] divImage full" style={{ backgroundImage: "url(/images/zapLogo.webp)" }} href="https://wa.me/12987024097?text=Olá!+Eu+gostaria+de+saber+mais+sobre+os+seus+produtos"></Link>
    );
 }
 
