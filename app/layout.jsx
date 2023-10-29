70895c207dd8b4cb3e63c59abf0c9872b95bf503
João Vitor de Aquino <jvaquinocontact@gmail.com>
2023-10-29 16:54:09 -0300

    + improve desc

--- app/layout.jsx
+++ app/layout.jsx
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
 
 export const metadata = {
   title: 'WLG - O melhor Distribuidor Pet do Vale do Paraíba',
-  description: 'Site do distribuidor de produtos para pets de empresas como Traty Vet, Propetz, KBSPK e Minag da marca WLG Distribuidor pet',
+  description: 'Site do distribuidor de produtos para pets de empresas como Traty Vet, Propetz, KBSPK e Minag da marca WLG Distribuidor pet. Atuamos em Guaratinguetá e em todo Vale do Paraíba ajudando seu pet shop diariamente.',
   applicationName: 'WLG Distribuidor Pet',
   authors: [{ name: "Code Fusion Agency", url: "https://code-fusion-alpha.vercel.app/" }],
   keywords: 'distribuidor pet, pets, traty vet, tratyvet, propetz, kbspk, minag, distribuidora pet, distribuidor pet vale do paraíba, produtos para pets',
